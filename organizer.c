ae954abb9900119c43d8c0fc3d19d0740b026307
NunoFerreiraCosta <nuno.costa7r@gmail.com>
2026-04-11 02:01:54 +0200

    Handling for user input incorrectness

--- organizer.c
+++ organizer.c
@@ -2,5 +2,11 @@
 
 int main (int argc, char *argv[])
 {
-    
+    // Handle user input incorrectness
+    if (argc < 2 || argc > 4)
+    {
+        printf("Usage: ./organizer /folder\n");
+        printf("       ./organizer --preview /folder\n");
+        return 1;
+    }
 }
